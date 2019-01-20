edf2ff4690f05ef550ff9d1759c736e84b91cb59
liquidjoo <liquidjoo@gmail.com>
2019-01-20 21:51:15 +0900

    init project

---
+++ src/main/java/io/bluemoon/product/category/GoogleProductCategoryApplication.java
@@ -0,0 +1,14 @@
+package io.bluemoon.product.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleProductCategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleProductCategoryApplication.class, args);
+    }
+
+}
+
