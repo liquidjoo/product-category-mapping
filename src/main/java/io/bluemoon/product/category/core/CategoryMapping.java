6a75ca6e249a47da99fa1298e0058ea91a705142
liquidjoo <liquidjoo@gmail.com>
2019-01-22 01:02:30 +0900

    resource loader change

--- src/main/java/io/bluemoon/product/category/core/CategoryMapping.java
+++ src/main/java/io/bluemoon/product/category/core/CategoryMapping.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -15,16 +15,22 @@ import java.util.Map;
 
 @Component
 public class CategoryMapping {
-    @Autowired
-    private ResourceLoader resourceLoader;
+
+    private Resource resource;
+
+    public CategoryMapping() {
+        final DefaultResourceLoader defaultResourceLoader = new DefaultResourceLoader();
+        this.resource = defaultResourceLoader.getResource("classpath:category_mapping.json");
+    }
 
     public String mapping() {
+
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> map = new HashMap<>();
 
 
         try {
-            String jsonPath = resourceLoader.getResource("classpath:category_mapping.json").getURI().getPath();
+            String jsonPath = resource.getURI().getPath();
             map = objectMapper.readValue(new File(jsonPath), new TypeReference<Map<String, Object>>(){});
             Map<String, Object> test = (Map<String, Object>) map.get("OUTER");
 
